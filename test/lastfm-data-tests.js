2bb1e0d99c241be0049fca06d67d5211f7498731
Hugo Almeida <hugornda@gmail.com>
2019-05-16 13:44:05 +0100

    Tests use promises

--- test/lastfm-data-tests.js
+++ test/lastfm-data-tests.js
@@ -11,23 +11,24 @@ const lastFMData = require('../data/lastfm-data').init(api_info)
 describe('LastFm - data Tests', () => {
 
     it('should get all artist with ffd', done => {
-        lastFMData.getArtistsByName('Fat Freddy s Drop', (err, data) => {
-            if (!err) {
-                expect(data)
-                    .to.be.an('array')
+        lastFMData.getArtistsByName('Fat Freddy s Drop').then(
+            (data) => {
+                if (data) {
+                    expect(data)
+                        .to.be.an('array')
 
-                assert.equal(data[0].name, "Fat Freddy's Drop")
-                assert.equal(data[0].mbid, "d451395a-f768-432e-bb70-d38c32baf4cb")
-                assert.equal(data[1].name, "Skream / Fat Freddy's Drop")
-                assert.equal(data[1].mbid, "")
-                done()
-            }
-        })
+                    assert.equal(data[0].name, "Fat Freddy's Drop")
+                    assert.equal(data[0].mbid, "d451395a-f768-432e-bb70-d38c32baf4cb")
+                    assert.equal(data[1].name, "Skream / Fat Freddy's Drop")
+                    assert.equal(data[1].mbid, "")
+                    done()
+                }
+            })
     })
 
     it('should get a albums of an artist', done => {
-        lastFMData.getAlbumsByMbid("d451395a-f768-432e-bb70-d38c32baf4cb", (err, data) => {
-            if (!err) {
+        lastFMData.getAlbumsByMbid("d451395a-f768-432e-bb70-d38c32baf4cb").then((data) => {
+            if (data) {
                 expect(data)
                     .to.be.an('array')
                     .and.to.have.length(8)
@@ -40,16 +41,17 @@ describe('LastFm - data Tests', () => {
     })
 
     it('should get tracks from an album', done => {
-        lastFMData.getTracksByMbid("0b3d401e-aa43-3e84-9b9b-51e0b67bce8a", (err, data) => {
-            if (!err) {
-                expect(data)
-                    .to.be.an('object')
-                assert.equal(data.tracks.track[0].name, 'Ernie')
-                assert.equal(data.tracks.track[0].duration, '437')
-                assert.equal(data.tracks.track[1].name, "Cay's Crays")
-                assert.equal(data.tracks.track[1].duration, '427')
-                done()
-            }
-        })
+        lastFMData.getTracksByMbid("0b3d401e-aa43-3e84-9b9b-51e0b67bce8a").then(
+            (data) => {
+                if (data) {
+                    expect(data)
+                        .to.be.an('object')
+                    assert.equal(data.tracks.track[0].name, 'Ernie')
+                    assert.equal(data.tracks.track[0].duration, '437')
+                    assert.equal(data.tracks.track[1].name, "Cay's Crays")
+                    assert.equal(data.tracks.track[1].duration, '427')
+                    done()
+                }
+            })
     })
 })
--- test/yama-db-tests.js
+++ test/yama-db-tests.js
@@ -7,82 +7,95 @@ const YamaDb = require('../data/yama-db')
 describe('Yama-db Tests', () => {
 
     const es = {
-        'host':'localhost',
-		'port':9200,
-		'index': 'yama'
+        'host': 'localhost',
+        'port': 9200,
+        'index': 'yama'
     }
     let client
 
     beforeEach(() => {
-        client = YamaDb.init(es)     
+        client = YamaDb.init(es)
     })
 
     it('init yamadb with the correct index uri', done => {
         assert.equal(client.uri, 'http://localhost:9200/yama')
         done()
     })
-    
+
     it('create a playlist', done => {
         let pl = {
-			"name": "g17 ",
-			"description": "my list"
-		}
-        client.createPlaylist(pl, (err, data) => {
-            if(!err) {
-                expect(data).to.be.an('object').and.have.a.property('status', 'created')
-                expect(data).to.have.a.property('_id')
-                client.getPlaylistInfo(data._id, (err, p) => {
-                    if(!err) {
-                        expect(p).to.be.an('object').and.have.a.property('name', pl.name)
-                        expect(p).to.have.a.property('description', pl.description)
-						expect(p).to.have.a.property('duration', pl.duration)
-                        expect(p).to.have.a.property('musics').that.is.an('array').and.have.length(0)                  
-                        client.deletePlaylist(data._id, (err, data) => {})
-                        done()
-                    }
-                })
-            }
-        })
+            "name": "g17 ",
+            "description": "my list"
+        }
+        client.createPlaylist(pl).then(
+            (data) => {
+                if (data) {
+                    expect(data).to.be.an('object').and.have.a.property('status', 'created')
+                    expect(data).to.have.a.property('_id')
+                    client.getPlaylistInfo(data._id)
+                        .then(
+                            (p) => {
+                                if (data) {
+                                    expect(p).to.be.an('object').and.have.a.property('name', pl.name)
+                                    expect(p).to.have.a.property('description', pl.description)
+                                    expect(p).to.have.a.property('duration', pl.duration)
+                                    expect(p).to.have.a.property('musics').that.is.an('array').and.have.length(0);
+
+                                }
+                            })
+                        .then(
+                            (p) => {
+                                client.deletePlaylist(data._id)
+                                done()
+                            })
+
+                }
+            })
     })
-    
+
     it('delete a playlist', done => {
         let pl = {
             "name": "g17 ",
-			"description": "my list"
+            "description": "my list"
         }
-        client.createPlaylist(pl, (err, data) => {
-            if(!err) {
-                client.deletePlaylist(data._id, (err, data) => {
-                    if(!err){
-                        expect(data).to.be.an('object').and.have.a.property('status', 'deleted')
-                        done()
-                    }
-                })
-            }
-        })
+        client.createPlaylist(pl).then(
+            (data) => {
+                if (data) {
+                    client.deletePlaylist(data._id).then(
+                        (data) => {
+                            if (data) {
+                                expect(data).to.be.an('object').and.have.a.property('status', 'deleted')
+                                done()
+                            }
+                        })
+                }
+            })
     })
 
     it('update a playlist', done => {
         let pl = {
             "name": "g17 ",
-			"description": "my list"
+            "description": "my list"
         }
-        client.createPlaylist(pl, (err, data) => {
-            let _id = data._id
-            if(!err) {
-                client.editPlaylist(_id, pl, (err, data) => {
-                    if (!err) {
-                        expect(data).to.be.an('object').and.have.a.property('status', 'updated')
-                        client.getPlaylistInfo(_id, (err, p) => {
-                            if (!err) {
-                                expect(p).and.have.a.property('description', 'my list')
-                                client.deletePlaylist(_id, (err, data) => {})
-                                done()
+        client.createPlaylist(pl).then(
+            (data) => {
+                let _id = data._id
+                if (data) {
+                    client.editPlaylist(_id, pl).then(
+                        (data) => {
+                            if (data) {
+                                expect(data).to.be.an('object').and.have.a.property('status', 'updated')
+                                client.getPlaylistInfo(_id).then(
+                                    (p) => {
+                                        if (data) {
+                                            expect(p).and.have.a.property('description', 'my list')
+                                            client.deletePlaylist(_id).then((data) => { })
+                                            done()
+                                        }
+                                    })
                             }
                         })
-                    }
-                })
-            }
-        })
+                }
+            })
     })
 })
